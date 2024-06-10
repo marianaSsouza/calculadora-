dc34d9e46815b862e82e93f13ec3de6c60dd7cae
Mariana <marianabatistella3@gmail.com>
2024-06-09 22:18:05 -0300

    retirei as aspas unicas nos cases do switch

--- src/App.java
+++ src/App.java
@@ -8,15 +8,52 @@ public class App {
         Scanner leitor = new Scanner(System.in);
 
         System.out.println("informe o primeiro numero: ");
-        double primeiroNUmero = leitor.nextDouble();
+        double primeiroNumero = leitor.nextDouble();
 
-        System.out.println("certo, agora informe o segundo numero: ");
+        System.out.println("informe o segundo numero: ");
         double segundoNumero = leitor.nextDouble();
 
-        System.out.println("informe qual operação deseja calcular ( +, - , *, /): ");
-        char operacao = leitor.next().charAt(0);
-        
+        System.out.println("informe qual operação deseja calcular:\n "
+                + "1) somar\n"
+                + "2) diminuir\n"
+                + "3) multiplicar\n"
+                + "4) dividir\n");
+        int operacao = leitor.nextInt();
 
+        double resultado = 0;
+
+        switch (operacao) {
+            case 1:
+                resultado = primeiroNumero + segundoNumero;
+
+                break;
+
+            case 2:
+                resultado = primeiroNumero - segundoNumero;
+
+                break;
+
+            case 3:
+                resultado = primeiroNumero * segundoNumero;
+
+                break;
+
+            case 4:
+                if (segundoNumero != 0) {
+                    resultado = primeiroNumero / segundoNumero;
+
+                } else {
+                    System.out.println("erro! informe um divisor diferente de zero");
+                    return;
+                }
+                break;
+
+            default:
+                System.out.println("operação invalida, tente novamente");
+                return;
+
+        }
+        System.out.println("o resultado da operação escolhida (" + operacao + ") é: " + resultado);
 
     }
 }
